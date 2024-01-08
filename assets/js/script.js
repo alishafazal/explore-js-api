8e597631badec7712d8ca7777536a570d28affd2
alisha fazal <alisha_fazal@hotmail.com>
2024-01-08 16:14:11 +0000

    Add API key, add basic css an html

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const API_KEY = "n1BP34ZObytBG7WImXYTU4uJP_g";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    } else {
+        throw new Error(data.error);
+    }
+}
